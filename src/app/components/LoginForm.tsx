a2fa4ead3cbfeae1e306a10a3f17e8400049b593
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2024-09-17 13:19:03 +0700

    feat: ui login

--- src/app/components/LoginForm.tsx
+++ src/app/components/LoginForm.tsx
@@ -1,1 +1,41 @@
-// use 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+
+export default function LoginForm() {
+  return (
+    <div className="w-screen min-h-screen flex items-center justify-center bg-gray-100">
+      <Card className="w-full max-w-[436px]">
+        <CardHeader className="flex justify-center items-center mb-12">
+          <img
+            src="images/logo.svg"
+            alt="logo"
+            className="w-[175px] h-[70px]"
+          />
+        </CardHeader>
+        <CardContent>
+          <h2 className="text-2xl font-semibold text-center mb-6">Login</h2>
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Input
+                type="text"
+                placeholder="Username"
+                className="w-full px-3 py-2 border rounded-md"
+              />
+            </div>
+            <div className="space-y-2">
+              <Input
+                type="password"
+                placeholder="Password"
+                className="w-full px-3 py-2 border rounded-md"
+              />
+            </div>
+            <Button className="w-full bg-[#000072] text-white py-2 rounded-md hover:bg-indigo-700 transition duration-300">
+              Sign In
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,16 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/login/Login';
 
 export default function App() {
   return (
     <div className='App'>
       <BrowserRouter>
       <Routes>
-        <Route path='/' element={<LandingPage />} />
-        </Routes>
+        <Route path='/' element={<LoginPage />} />
+        <Route path='login' element={<LoginPage />} />
+      </Routes>
       </BrowserRouter>
     </div>
   );
