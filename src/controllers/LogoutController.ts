e1deee1d77f3db42a32f53a50a7b49846378c7de
Muhammad Malik Ibrahim Baharsyah <13521029@mahasiswa.itb.ac.id>
2024-10-07 15:43:38 +0700

    feat: fix bugs

--- src/controllers/LogoutController.ts
+++ src/controllers/LogoutController.ts
@@ -1,10 +1,9 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export const logout = async (req: NextRequest) => {
+export const logout = async () => {
     try {
-        // Clear the cookie
         return NextResponse.json({ message: "Successfully logged out" }, { status: 200 });
     } catch (error) {
-        return NextResponse.json({ error: error.message }, { status: 500 });
+        return NextResponse.json({ message: "Failed to log out" }, { status: 500 });
     }
 }
