ebede161f7a2543a26ead7a120b51f052245b4e3
Muhammad Malik Ibrahim Baharsyah <13521029@mahasiswa.itb.ac.id>
2024-10-14 11:18:19 +0700

    fix confetti bug

--- src/app/(pages)/lottery/StartLotteryPage.tsx
+++ src/app/(pages)/lottery/StartLotteryPage.tsx
@@ -6,11 +6,12 @@ import { Slider } from "@/app/components/ui/slider";
 import Confetti from 'react-confetti'
 import Image from 'next/image';
 import useFetchAPI from '@/app/components/hooks/fetchAPI';
+import { useWindowSize } from 'react-use';
 
 export default function StartLotteryPage(): JSX.Element {
-    const { setStep, step, qty, setQty, selectedPrize, selectedEvent } = useLottery()
+    const { setStep, step, qty, setQty, selectedPrize, selectedEvent } = useLottery();
 
-    const [participants, setParticipants] = useState<string[]>([])
+    const [participants, setParticipants] = useState<string[]>([]);
     const [currentParticipants, setCurrentParticipants] = useState<string[]>([]);
     const [isShuffling, setIsShuffling] = useState(false);
     const [speed, setSpeed] = useState(126);
@@ -19,6 +20,7 @@ export default function StartLotteryPage(): JSX.Element {
     const fetchAPI = useFetchAPI();
     const startSoundRef = useRef<HTMLAudioElement | null>(null);
     const stopSoundRef = useRef<HTMLAudioElement | null>(null);
+    const { width, height } = useWindowSize();
 
     const shuffleParticipants = (array: string[]) => {
         const shuffled = [...array];
@@ -94,46 +96,47 @@ export default function StartLotteryPage(): JSX.Element {
 
     return (
       <>
-      {showConfetti && <Confetti />}
-        <div className="p-6 space-y-6 flex-1 flex flex-col font-poppins">
+        <div className="p-6 space-y-6 flex-1 flex flex-col font-poppins relative overflow-hidden h-screen">
+            {showConfetti && <Confetti width={width} height={height} numberOfPieces={250} />}
             <h1 className="text-3xl font-bold text-[#000072]">{selectedEvent?.name}</h1>
-            <div className="mb-6 flex flex-col items-center justify-center">
-                <h2 className="text-4xl font-bold text-center mb-4 mt-8">{selectedPrize?.name}</h2>
-                <div className="relative w-full max-w-2xl flex justify-center">
-                    <div className="absolute inset-0 bg-blue-300 rounded-full filter blur-3xl opacity-50"></div>
-                    <Image
-                        loader={({ src }) => src}
-                        src={`${process.env.NEXT_PUBLIC_API_BASE_URL}/${selectedPrize?.image}`}
-                        alt="Prize"
-                        width={320}
-                        height={240}
-                        className="relative rounded-lg max-w-[300px] max-h-[300px]"
-                    />
+            <div className="overflow-auto border-y flex-1">
+                <div className="mb-6 flex flex-col items-center justify-center">
+                    <h2 className="text-4xl font-bold text-center mb-4 mt-8">{selectedPrize?.name}</h2>
+                    <div className="relative w-full max-w-2xl flex justify-center">
+                        <div className="absolute inset-0 bg-blue-300 rounded-full filter blur-3xl opacity-50"></div>
+                        <Image
+                            loader={({ src }) => src}
+                            src={`${process.env.NEXT_PUBLIC_API_BASE_URL}/${selectedPrize?.image}`}
+                            alt="Prize"
+                            width={320}
+                            height={240}
+                            className="relative rounded-lg max-w-[300px] max-h-[300px]"
+                        />
+                    </div>
+                </div>
+                <div className="grid gap-6 mb-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 place-items-center justify-items-center">
+                    {currentParticipants.map((participant, index) => (
+                        <div key={index} className="w-full max-w-[280px]">
+                            <WinnerButton initialId={participant} isShuffling={isShuffling} />
+                        </div>
+                    ))}
                 </div>
             </div>
-            <div className="grid flex-1 gap-6 mb-6 w-full grid-cols-1 sm:grid-cols-2 md:grid-cols-3 place-items-center justify-items-center">
-                {currentParticipants.map((participant, index) => (
-                    <div key={index} className="w-full max-w-[280px]">
-                        <WinnerButton initialId={participant} isShuffling={isShuffling} />
-                    </div>
-                ))}
-            </div>
-            <div className="space-y-4 justify-center items-center flex flex-col">
-                <h2 className="text-xl font-semibold">Speed</h2>
-                    <Slider
-                        defaultValue={[125]}
-                        onValueChange={(value) => handleSpeedChange(value[0])}
-                        max={250}
-                        min={1}
-                        step={1}
-                        className='w-60'
-                        />
-                    <p>current Speed: {251 - speed}</p>
-            </div>
-            <div className="flex justify-between space-x-4">
-                <Button className='hover:bg-[#000072]/90 hover:text-white' variant="outline" onClick={() => { setStep(step - 1); setQty(0); }}>
+            <div className="flex justify-between space-x-4 items-end">
+                <Button className="hover:bg-[#000072]/90 hover:text-white" variant="outline" onClick={() => { setStep(step - 1); setQty(0); }}>
                     Choose Prize
                 </Button>
+            <div className="space-y-3 justify-center items-center flex flex-col">
+                <Slider
+                    defaultValue={[125]}
+                    onValueChange={(value) => handleSpeedChange(value[0])}
+                    max={250}
+                    min={1}
+                    step={1}
+                    className="w-60"
+                />
+                <p>Current Speed: {251 - speed}</p>
+            </div>
                 <Button className="bg-[#000072] hover:bg-[#000072]/90 text-white"
                     onClick={() => { isShuffling ? handleStopShuffle() : handleStartShuffle(); setIsShuffling(!isShuffling) }}
                 >
