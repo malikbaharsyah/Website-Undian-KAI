21f96878133a4d1e2eef41ff41c54efe5cce74c7
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2024-10-25 14:43:08 +0700

    feat: add sign out button on dropdown

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -15,7 +15,9 @@ import {
   LogOutIcon,
   ChevronLeftIcon,
   ChevronRightIcon,
+  ChevronUpIcon
 } from "lucide-react";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@/app/components/ui/dropdown-menu";
 
 export default function Sidebar() {
   const [isOpen, setIsOpen] = useState(true);
@@ -52,9 +54,7 @@ export default function Sidebar() {
     localStorage.setItem("isSidebarOpen", newState.toString());
   };
 
-  const sidebarClassName = `${
-    isOpen ? "w-64" : "w-20"
-  } font-poppins min-h-screen bg-[#F3F3F3] border-r border-gray-200 flex flex-col transition-all duration-500 ease-in-out relative`;
+  const sidebarClassName = `${isOpen ? "w-64" : "w-20"} font-poppins min-h-screen bg-[#F3F3F3] border-r border-gray-200 flex flex-col transition-all duration-500 ease-in-out relative`;
 
   return (
     <div className={isFirstRender.current ? "font-poppins min-h-screen bg-[#F3F3F3] border-r border-gray-200 flex flex-col relative" : sidebarClassName}>
@@ -75,15 +75,13 @@ export default function Sidebar() {
       </Button>
       <div className={`p-4 ${isOpen ? "" : "justify-center"}`}>
         <div
-          className={`flex items-center ${
-            isOpen ? "space-x-3" : "justify-center"
-          }`}
+          className={`flex items-center ${isOpen ? "space-x-3" : "justify-center"}`}
         >
           <Image
             src="/images/logo.svg"
             alt="logo"
             width={120}
-            height={50}            
+            height={50}
             className="p-2"
           />
         </div>
@@ -121,58 +119,44 @@ export default function Sidebar() {
         </ul>
       </nav>
       <div className={`p-4 ${isOpen ? "" : "flex justify-center"}`}>
-        <div className="flex items-center content-center">
-          <Avatar className="items-center justify-center">
-            <CircleUser
-              strokeWidth={1.2}
-              size={32}
-              color="#374151"
-            />
-          </Avatar>
-          {isOpen && (
-            <span className="text-lg text-gray-700 font-medium pl-2">{username}</span>
-          )}
-        </div>
-        {/* <Button
-          variant="ghost"
-          className={`${
-            isOpen ? "w-full justify-start" : "w-auto p-2"
-          } text-gray-700 hover:bg-gray-100`}
-          asChild
-        >
-          <Link href="/logout" className="flex items-center space-x-3">
-            <LogOutIcon className="h-5 w-5" />
-            {isOpen && <span>Logout</span>}
-          </Link>
-        </Button> */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <div className="flex items-center cursor-pointer hover:bg-gray-200 p-2 rounded-md">
+              <Avatar className="items-center justify-center">
+                <CircleUser strokeWidth={1.2} size={32} color="#374151" />
+              </Avatar>
+              {isOpen && (
+                <span className="text-lg text-gray-700 font-medium pl-2">{username}</span>
+              )}
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end" className="bg-white border rounded-md p-1 shadow-lg">
+            <DropdownMenuItem asChild>
+              <Link href="/logout" className="flex items-centerp-2 rounded-md">
+                <LogOutIcon className="h-5 w-5"
+                color="#ef4444"
+                />
+                <span className="pl-2 text-red-600">Logout</span>
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
 }
 
-function NavItem({
-  href,
-  icon,
-  text,
-  isOpen,
-  isActive = false,
-}: NavItemProps) {
+function NavItem({ href, icon, text, isOpen, isActive = false }: NavItemProps) {
   return (
     <Link
       href={href}
-      className={`flex items-center ${
-        isOpen ? "space-x-3" : "justify-center"
-      } ${
-        isActive
-          ? "text-[#000072] font-medium"
-          : "text-[#333333]/[0.6] hover:text-[#333333] hover:font-medium"
+      className={`flex items-center ${isOpen ? "space-x-3" : "justify-center"} ${
+        isActive ? "text-[#000072] font-medium" : "text-[#333333]/[0.6] hover:text-[#333333] hover:font-medium"
       } rounded-lg p-2 transition-colors duration-300`}
     >
       {icon}
       {isOpen && (
-        <span className="w-36 overflow-hidden text-ellipsis whitespace-nowrap">
-          {text}
-        </span>
+        <span className="w-36 overflow-hidden text-ellipsis whitespace-nowrap">{text}</span>
       )}
     </Link>
   );
